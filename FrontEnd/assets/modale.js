a2287edf3297094dd082af5e23fe82c1ee2d7825
Solweg <lngouguenheim@hotmail.fr>
2024-05-04 17:07:20 +0200

    boutton modale ok

--- FrontEnd/assets/modale.js
+++ FrontEnd/assets/modale.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const loginlogout = document.getElementById("loginlogout");
   const ContainerFilters = document.getElementById("ContainerFilters");
   const banner = document.getElementById("banner");
-  const edit = document.getElementById("edit");
+  const edit = document.getElementById("groupedit");
 
   if (sessionStorage.getItem("token")) {
     loginlogout.textContent = "logout";
@@ -21,3 +21,27 @@ document.addEventListener("DOMContentLoaded", () => {
     edit.style.display = "none";
   }
 });
+
+
+
+// Obtient la référence des éléments
+const modal = document.getElementById("myModal");
+const modifier = document.getElementById("groupedit");
+const span = document.getElementsByClassName("close")[0];
+
+// Associe un gestionnaire d'événements pour ouvrir la modal lorsque le bouton est cliqué
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Associe un gestionnaire d'événements pour fermer la modal lorsque l'utilisateur clique sur la croix
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Associe un gestionnaire d'événements pour fermer la modal lorsque l'utilisateur clique en dehors de celle-ci
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
